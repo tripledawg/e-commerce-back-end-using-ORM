2cca062381a844758d6b552404055e92cb6e5dc3
tripledawg <iamlesliecrouch>
2022-02-01 14:50:29 -0500

    added one to many methods to table classes

--- models/index.js
+++ models/index.js
@@ -3,14 +3,30 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { belongsTo } = require('./Product');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'id',
+  //correct?? 
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'id',
+  //correct?? 
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'id,'//'id'?
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,
+  {
+    foreignKey: 'id'//'id'?
+  })
 
 module.exports = {
   Product,
